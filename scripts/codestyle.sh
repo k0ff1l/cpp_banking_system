a52dea3e8f5f1c77ad5a2314d68463e6b9cff47c
k0ff1l <filka@workmgmt.ru>
2023-12-06 16:22:41 +0300

    переместил файлы accounts.cpp/.h и processing.cpp/.h по папкам + фикс скрипта и кодстайла.

--- scripts/codestyle.sh
+++ scripts/codestyle.sh
@@ -6,6 +6,6 @@ set -e
 
 declare -r FILTER=-build/include_subdir
 
-grep -E -r --include=\*.{cpp,h,hpp} -L 'cmake-build-debug'| xargs python3 -m cpplint --linelength=120 --filter=$FILTER --output=vs7
+grep -E -r --include=\*.{cpp,h,hpp} -L 'cmake-build-debug' ./src/* | xargs python3 -m cpplint --linelength=120 --filter=$FILTER --output=vs7
 
 popd
