9f556e9a675494128dffd3126570f78de0975083
k0ff1l <filka@workmgmt.ru>
2023-12-08 18:23:18 +0300

    updated filters for stylecheck, -runtime/refrences

--- scripts/codestyle.sh
+++ scripts/codestyle.sh
@@ -4,7 +4,7 @@ pushd $(git rev-parse --show-toplevel)
 
 set -e
 
-declare -r FILTER=-build/include_subdir
+declare -r FILTER=-build/include_subdir,-runtime/references
 
 grep -E -r --include=\*.{cpp,h,hpp} -L 'cmake-build-debug' ./src/* ./tests/* | xargs python3 -m cpplint --linelength=120 --filter=$FILTER --output=vs7
 
--- src/accounts/accounts.cpp
+++ src/accounts/accounts.cpp
@@ -7,56 +7,58 @@
 #include <string>
 #include <utility>
 
- // TODO : on deposit check if >0;
- // TODO : on withdrawal check if money available >= withdrawing money;
+// TODO(fleurs) : on deposit check if >0;
+// TODO(fleurs) : on withdrawal check if money available >= withdrawing money;
 
- // id начинается с 0, но 0 id - служебный для ПУСТЫХ аккаунтов.
+// id начинается с 0, но 0 id - служебный для ПУСТЫХ аккаунтов.
 
- namespace bank{
- class user_db{
-   user_db(){
-     // open connection
-     // total_active_accounts_ = size(all_users); // каким-то образом
-     // init all_users;
-     // init prev_id = id of last user of db;
-   }
+namespace bank {
+class user_db {
+  user_db() {
+    // open connection
+    // total_active_accounts_ = size(all_users); // каким-то образом
+    // init all_users;
+    // init prev_id = id of last user of db;
+  }
 
-   ~user_db(){
-     // close connection
-     // show that user_db is closed
-   }
-   class user{
-    public:
-     user(){
-       id_ = user_db::prev_id_ + 1;
-       user_db::prev_id_ = id_;
-       money_ = 0;
-     }
+  ~user_db() {
+    // close connection
+    // show that user_db is closed
+  }
+  class user {
+   public:
+    user() {
+      id_ = user_db::prev_id_ + 1;
+      user_db::prev_id_ = id_;
+      money_ = 0;
+    }
 
-     user(unsigned int money, std::string& name) : money_(money), name_(std::move(name)){
-       id_ = user_db::prev_id_ + 1;
-       user_db::prev_id_ = id_;
-       ++user_db::total_active_accounts_;
-     }
+    user(unsigned int money, std::string &name)
+        : money_(money), name_(std::move(name)) {
+      id_ = user_db::prev_id_ + 1;
+      user_db::prev_id_ = id_;
+      ++user_db::total_active_accounts_;
+    }
 
-     user(user &other) = delete;
+    user(user &other) = delete;
 
-     user(user &&other) = delete;
+    user(user &&other) = delete;
 
-     ~user(){
-       id_ = 0;
-       money_ = 0;
-       --user_db::total_active_accounts_;
-     }
+    ~user() {
+      id_ = 0;
+      money_ = 0;
+      --user_db::total_active_accounts_;
+    }
 
-    private:
-     unsigned int id_;
-     unsigned int money_;
-     std::string name_;
-   };
+   private:
+    unsigned int id_;
+    unsigned int money_;
+    std::string name_;
+  };
 
-   static unsigned int total_active_accounts_;
-   static std::unordered_map<unsigned int, user> all_users; // key: id, pred: user
-   static unsigned int prev_id_;
- };
-}
+  static unsigned int total_active_accounts_;
+  static std::unordered_map<unsigned int, user>
+      all_users;  // key: id, pred: user
+  static unsigned int prev_id_;
+};
+}  // namespace bank
